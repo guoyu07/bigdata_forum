2e41b8b225c32bd852d462d122706ad15af5bb92
Henry Lee <manageyp@gmail.com>
2013-08-30 19:15:49 +0800

    [Change] unicorn config

--- config/unicorn.rb
+++ config/unicorn.rb
@@ -1,11 +1,10 @@
 module Rails
   class <<self
     def root
-      File.expand_path("../..", __FILE__)
+      File.expand_path(__FILE__).split('/')[0..-3].join('/')
     end
   end
 end
-puts Rails.root
 rails_env = ENV["RAILS_ENV"] || "development"
 
 preload_app true
@@ -16,7 +15,6 @@ stdout_path "#{Rails.root}/log/unicorn.log"
 
 listen 3000, :tcp_nopush => false
 
-listen "/tmp/unicorn.ruby-china.sock"
 worker_processes 1
 timeout 120
 
@@ -25,10 +23,16 @@ if GC.respond_to?(:copy_on_write_friendly=)
 end
 
 before_exec do |server|
-  ENV["BUNDLE_GEMFILE"] = "#{Rails.root}/Gemfile"
+  ENV['BUNDLE_GEMFILE'] = "#{Rails.root}/Gemfile"
 end
 
 before_fork do |server, worker|
+  # the following is highly recomended for Rails + "preload_app true"
+  # as there's no need for the master process to hold a connection
+  if defined?(ActiveRecord::Base)
+    ActiveRecord::Base.connection.disconnect!
+  end
+
   old_pid = "#{Rails.root}/tmp/pids/unicorn.pid.oldbin"
   if File.exists?(old_pid) && server.pid != old_pid
     begin
@@ -37,4 +41,17 @@ before_fork do |server, worker|
       puts "Send 'QUIT' signal to unicorn error!"
     end
   end
+end
+
+after_fork do |server, worker|
+  # the following is *required* for Rails + "preload_app true",
+  if defined?(ActiveRecord::Base)
+    ActiveRecord::Base.establish_connection
+  end
+
+  # if preload_app is true, then you may also want to check and
+  # restart any other shared sockets/descriptors such as Memcached,
+  # and Redis.  TokyoCabinet file handles are safe to reuse
+  # between any number of forked children (assuming your kernel
+  # correctly implements pread()/pwrite() system calls)
 end
