5739ce3db1301220ea9682662423297b7cc2366a
Henry Lee <manageyp@gmail.com>
2013-08-30 16:58:26 +0800

    [Change] default unicorn port 3000

--- config/unicorn.rb
+++ config/unicorn.rb
@@ -14,7 +14,7 @@ pid "#{Rails.root}/tmp/pids/unicorn.pid"
 stderr_path "#{Rails.root}/log/unicorn.log"
 stdout_path "#{Rails.root}/log/unicorn.log"
 
-listen 5000, :tcp_nopush => false
+listen 3000, :tcp_nopush => false
 
 listen "/tmp/unicorn.ruby-china.sock"
 worker_processes 6
